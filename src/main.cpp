642fe61ccbf03fe09b1ff6cfbd8c2ad06954b8dd
GJTiquia <gershom.tiquia@gmail.com>
2022-02-20 16:11:28 +0800

    cleaned main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
 #include <Arduino.h>
 
-// Fill-in information from your Blynk Template here
 #define BLYNK_TEMPLATE_ID           "TMPLOZYkdSFA"
 #define BLYNK_DEVICE_NAME           "HADES_ESP32"
-
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
-
 #define BLYNK_PRINT Serial
 //#define BLYNK_DEBUG
 
 #define APP_DEBUG
 
 // Uncomment your board, or configure a custom board in Settings.h
-//#define USE_WROVER_BOARD
-//#define USE_TTGO_T7
-
 #include "BlynkEdgent.h"
 
 void setup()
