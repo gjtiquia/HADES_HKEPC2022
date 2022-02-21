5eef7a20a79a883b0993107f858d3aadef61bb71
GJTiquia <gershom.tiquia@gmail.com>
2022-02-21 13:20:00 +0800

    working test connection button and terminal widget update

--- src/main.cpp
+++ src/main.cpp
@@ -18,27 +18,51 @@
 #define V_SPRAYING V4
 #define V_TEST_SPRAY V5
 #define V_TERMINAL V6
+#define V_TEST_CONNECTION V7
+
+#define LED_PIN 2
 
 WidgetTerminal terminal(V6);
 
 double sprayTime;
 
 BLYNK_CONNECTED() {
+    terminal.println("----------------");
     terminal.println("Device Connected");
+    terminal.flush();
     Blynk.syncAll();
 }
 
 BLYNK_WRITE(V_SPRAYTIME) {
   sprayTime = param.asDouble();
   Serial.print((String) "Spray Time: " + sprayTime + "s\n");
+  terminal.println((String) "Device Updated Spray Time: " + sprayTime + "s");
+  terminal.flush();
+}
+
+BLYNK_WRITE(V_TEST_CONNECTION) {
+  int button = param.asInt();
+
+  Serial.print((String) "Test Button Pressed: " + button + "\n");
+  terminal.println((String) "Device Received Test Button Press: " + button);
+  terminal.flush();
+
+  if (button == 0) {
+    digitalWrite(LED_PIN, LOW);
+  }
+  else if (button == 1) {
+    digitalWrite(LED_PIN, HIGH);
+  }
 }
 
 void setup()
 {
   Serial.begin(115200);
   delay(100);
-
+  
   BlynkEdgent.begin();
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
